36f4fabe7050bc13243e23539dff39da1588c5c5
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-02-21 18:16:20 +0100

    Construcción base de la aplicación

---
+++ app-imperial/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModulesModule } from './modules/modules.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SkeletonComponent,
+    FooterComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ModulesModule,
+    SharedModule,
+    CoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

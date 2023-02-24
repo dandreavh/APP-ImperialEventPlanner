33cf492c65b013825495d5e28ba71166f35e8a42
Andrea Vieira Hernández <alum.dvieirah.2021@iesalixar.org>
2023-02-24 11:11:48 +0100

    Componente Home y primeros pasos de footer

--- app-imperial/src/app/app-routing.module.ts
+++ app-imperial/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SkeletonComponent,
+    pathMatch: 'prefix',
+    children: [
+      {path: '', component: HomeComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app-imperial/src/app/core/material/material.module.ts
+++ app-imperial/src/app/core/material/material.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [
-    
+    MatIconModule
   ]
 })
 export class MaterialModule { }
